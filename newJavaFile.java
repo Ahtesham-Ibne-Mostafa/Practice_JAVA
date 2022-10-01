7309c6e66bc72dbb06deaf15e73d38810be5a457
Ahtesham Ibne Mostafa <ahteshamibnemostafa@gmail.com>
2022-10-02 00:44:34 +0600

    Update newJavaFile.java

---
+++ newJavaFile.java
@@ -0,0 +1,20 @@
+public class Arrays {
+    public static void main (String[] args){
+        int []a = new int [6];
+        
+        
+        
+        a[0]=10;
+        a[1]=20;
+        a[2]=30;
+        a[3]=40;
+        a[4]=50;
+        a[5]=60;
+        
+        System.out.println(a[0]);
+        
+        for (int i=0; i<6 ; i++)
+            System.out.println(a[i]);
+    }
+    
+}
